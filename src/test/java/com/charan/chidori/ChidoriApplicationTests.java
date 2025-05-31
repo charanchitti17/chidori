d7a5d8246e589fa0b311dac2711232f1ae15579e
charanchitti177 <adhavenisaicharan@gmail.com>
2025-05-31 13:20:17 +0530

    Initial commit

---
+++ src/test/java/com/charan/chidori/ChidoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.chidori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChidoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
