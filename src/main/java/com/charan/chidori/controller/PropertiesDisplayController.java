380bf0f9fbe26692ef05588deb1141aaa972235f
charanchitti177 <adhavenisaicharan@gmail.com>
2025-07-13 21:09:09 +0530

    Initial application

--- src/main/java/com/charan/chidori/controller/PropertiesDisplayController.java
+++ src/main/java/com/charan/chidori/controller/PropertiesDisplayController.java
@@ -1,10 +1,15 @@
 package com.charan.chidori.controller;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/properties")
 public class PropertiesDisplayController {
@@ -14,6 +19,11 @@ public class PropertiesDisplayController {
 
     @Value("${db.name:default-db}")
     private String dbName;
+
+    @PostConstruct
+    public void init() {
+        log.info("Bean initialized with appName {}, dbName {}", appName, dbName);
+    }
 
     @GetMapping("/appName")
     public String displayApplicationName() {
